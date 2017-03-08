bf479316e0c49327cdbc1fb53310509a7f791807
Eddyding <920398694@qq.com>
2017-03-08 10:37:18 +0800

    judge if file or dir is exist is linux c

---
+++ file/dir_exist.c
@@ -0,0 +1,44 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<dirent.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+@breif: 判断目录是否存在
+@param: dir_path,目录名称，包含路径
+
+@return: 目录路径为空或者不存在目录返回-1，否则存在返回0
+
+*/
+
+int is_dir_exist(const char* dir_path)
+{
+	if(dir_path == NULL)
+		return -1;
+	if(opendir(dir_path) == NULL) 
+		return -1;
+	closedir((DIR *)dir_path); // balance
+	return 0;
+}
+
+
+int 
+main()
+{
+	int ret = -2;
+
+	char *home = "HOME";
+
+	printf("$HOME is %s\n",getenv(home));
+
+	if((ret = is_dir_exist(getenv(home))) == -1)
+		printf("dir is not exist or dir_path is wrong\n");
+	else
+		printf("dir is exist!\n");
+	if((ret = is_dir_exist("ssdir_exist.c")) == -1)
+		printf("ssdir_exist.c is not exist or dir_path is wrong\n");
+	else
+		printf("ssdir_exist.c is exist!\n");
+	return 0;
+}
---
+++ file/file_exist.c
@@ -0,0 +1,38 @@
+#include<unistd.h>
+#include<stdio.h>
+
+/*
+@breif: 判断文件是否存在
+@param: file_path,文件名称，包含路径
+
+@return: 文件路径为空或者不存在文件返回-1，否则存在返回0
+
+*/
+//int access(const char* pathname,int mode);
+
+
+int is_file_exist(const char* file_path)
+{
+	if(file_path == NULL)
+		return -1;
+	if(access(file_path,F_OK) == 0)
+		return 0;
+	return -1;
+}
+
+
+int 
+main()
+{
+	int ret = -2;
+
+	if((ret = is_file_exist("file_exist.c")) == -1)
+		printf("file is not exist or file_path is wrong\n");
+	else
+		printf("file is exist!\n");
+	if((ret = is_file_exist("ssfile_exist.c")) == -1)
+		printf("ssfile_exist.c is not exist or file_path is wrong\n");
+	else
+		printf("ssfile_exist.c is exist!\n");
+	return 0;
+}

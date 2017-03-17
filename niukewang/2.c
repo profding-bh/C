09ab1f0b8182e2b7dc65a3b06eb3911a9d020ee8
profding <1647262710@qq.com>
2017-03-17 11:09:13 +0800

    array tricky thing

--- niukewang/2.c
+++ niukewang/2.c
@@ -6,16 +6,24 @@ int
 main()
 {
 	int a[5] = {1,3,5,7,9};
-
-	for(int i = 0; i<5;++i)
+        int i;
+	for( i = 0; i<5;++i)
 		printf("%p\n",&a[i]);
 	printf("a is %p\n",a);
 	int *q = a;
 	
 	int* q2 = (int*)(q+1);
 	int* p = (int*)(&a+1); // 是加整个数组，而不是4
-	printf(" *p = %d,p is %p\n",*p,p);
+	int* p3 = (int*)(a+1); // 是加4，而不是 整个数组!!!好tricky.
+
+// &a+1 和 a+1是不一样的。
+// &a+1 是加整个数组，而a+1，是指针加4（8）
+
 	printf(" q2 = %p,*q2 is %d\n",q2,(*q2));
+	printf(" *p = %d,p is %p\n",*p,p);
+	printf(" *p3 = %d,p3 is %p\n",*p3,p3);
+
+
 	printf("%d,%d\n",*(a+1),*(p-1));
 	return 0;
 }

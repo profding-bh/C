32a768bba354c0ddd145a2f01f8945d91a651a5a
profding <Eddyding>
2017-10-17 17:09:33 +0800

    map pair

---
+++ niukewang/map/usemap.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<map>
+#include<string>
+
+
+using namespace std;
+
+
+//pair函数
+
+
+int 
+main()
+{
+	map<int,string> mapStudent;
+	
+	mapStudent.insert(pair<int,string>(1,"student_one"));
+	mapStudent.insert(pair<int,string>(2,"student_two"));
+	mapStudent.insert(pair<int,string>(3,"student_three"));
+
+	map<int,string>::iterator iter;
+	for(iter = mapStudent.begin();iter != mapStudent.end();iter++)
+		cout<<iter->first<<' '<<iter->second<<endl;
+	return 0;
+}
+
+
+
+
+

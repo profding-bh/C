171d82505823ff06019c606141adeb5c71241964
profding <Eddyding>
2017-10-17 15:15:32 +0800

    sizeof

--- niukewang/sizeofvoid.cpp
+++ niukewang/sizeofvoid.cpp
@@ -5,9 +5,29 @@ struct a
 {
 	
 };
+
+class b
+{
+};
+
+
+class c{
+c(){}
+~c(){}
+};
+
+
+class d{
+d(){}
+virtual ~d(){}
+};
+
 int 
 main()
 {
-    printf("%d\n",sizeof(a));	
+    printf("%d\n",sizeof(a));	//1
+	printf("%d\n",sizeof(b));	//1
+	printf("%d\n",sizeof(c)); // 1
+	printf("%d\n",sizeof(d)); // 4
 	return 0;
  } 

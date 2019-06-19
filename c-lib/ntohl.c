fc02cc57cbf4a8dad089595630287999d9701f77
profding-bh <920398694@qq.com>
2019-06-20 05:10:17 +0800

    c-lib

--- c-lib/ntohl.c
+++ c-lib/ntohl.c
@@ -6,11 +6,23 @@
  * netlong: 一个以网络字节序表达的32位数
  * 需要引入头文件：#include <arpa/inet.h>
  * 功能：将一个32位数由网络字节序（大端方式） 转换成主机字节序（小端方式）
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 int main()
 {
 	uint32_t a = ntohl(0x12345678);
 	printf("a is 0x%x\n",a);// 0x78563412
+
+	uint16_t b = ntohs(0x1234);
+	printf("b is 0x%x\n",b);// 0x3412
+
+	
 	return 0;
 }
 
